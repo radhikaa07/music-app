30c4346c182415a2dba4635f80b2b5066540287b
radhikaa07 <124397738+radhikaa07@users.noreply.github.com>
2024-02-17 12:14:23 +0530

    final

---
+++ app/src/main/java/com/example/music_app/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.music_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView text;;
+    Button start_btn, stop_btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        start_btn= findViewById(R.id.button);
+        stop_btn= findViewById(R.id.button2);
+        text= findViewById(R.id.textView);
+
+        start_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent serviceIntent = new Intent(getApplicationContext(),
+                        MyService.class);
+                startService(serviceIntent);
+            }
+        });
+
+        stop_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent serviceIntent = new Intent(getApplicationContext(),
+                        MyService.class);
+                stopService(serviceIntent);
+            }
+        });
+
+
+    }
+}
